948c1451aa65a7e8251f37b6d325bdd8385dcb05
Victor Salles <vsallesa@gmail.com>
2025-05-30 16:04:14 -0300

    test: integration test ensure database connection
    tests can validate whether the data was actually recorded

--- tests/subscription.rs
+++ tests/subscription.rs
@@ -1,10 +1,18 @@
 //! tests/subscription.rs
 
+use email_newsletter::configuration::get_configuration;
 use email_newsletter::utils::spawn_sut;
+use sqlx::{Connection, PgConnection};
 
 #[tokio::test]
 async fn subscribe_return_a_200_for_valid_form_data() {
     let sut_address = spawn_sut();
+    let configuration = get_configuration().expect("Failed to read configuration");
+    let connection_string = configuration.database.connection_string();
+    let mut connection = PgConnection::connect(&connection_string)
+        .await
+        .expect("Failed to connect to the database");
+
     let client = reqwest::Client::new();
 
     let body = "name=John%20Doe&email=john.doe%40example.com";
@@ -17,6 +25,14 @@ async fn subscribe_return_a_200_for_valid_form_data() {
         .expect("Failed to execute request.");
 
     assert_eq!(200, response.status().as_u16());
+
+    let saved = sqlx::query!("SELECT name, email FROM subscriptions",)
+        .fetch_one(&mut connection)
+        .await
+        .expect("Failed to fetch saved subscription");
+
+    assert_eq!(saved.name, "John Doe");
+    assert_eq!(saved.email, "john.doe@example.com");
 }
 
 #[tokio::test]
