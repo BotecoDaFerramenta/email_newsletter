908379df4a8550c0eaa3db36943bdbb00f774276
Victor Salles <vsallesa@gmail.com>
2025-06-04 01:17:21 -0300

    test: fix integration tests configuration

--- src/utils/spawn_sut.rs
+++ src/utils/spawn_sut.rs
@@ -1,9 +1,10 @@
 //! src/utils/test.rs
 
-use crate::configuration::get_configuration;
+use crate::configuration::{DatabaseSettings, get_configuration};
 use crate::startup::run;
-use sqlx::PgPool;
+use sqlx::{Connection, Executor, PgConnection, PgPool};
 use std::net::TcpListener;
+use uuid::Uuid;
 
 pub struct TestApp {
     pub address: String,
@@ -14,15 +15,35 @@ pub async fn spawn_sut() -> TestApp {
     let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
     let port = listener.local_addr().unwrap().port();
     let address = format!("http://127.0.0.1:{}", port);
-    let configuration = get_configuration().expect("Failed to read configuration");
-    let db_pool = PgPool::connect(&configuration.database.connection_string())
-        .await
-        .expect("Failed to connect to the database");
+
+    let mut configuration = get_configuration().expect("Failed to read configuration");
+    configuration.database.database_name = Uuid::new_v4().to_string();
+
+    let db_pool = configure_database(&configuration.database).await;
     let server = run(listener, db_pool.clone()).expect("Failed to bind address");
     #[allow(clippy::let_underscore_future)]
     let _ = tokio::spawn(server);
-    TestApp {
-        address: address.clone(),
-        db_pool,
-    }
+    TestApp { address, db_pool }
+}
+
+pub async fn configure_database(config: &DatabaseSettings) -> PgPool {
+    let mut connection = PgConnection::connect(&config.connection_string_without_db())
+        .await
+        .expect("Failed to connect to Postgres");
+
+    connection
+        .execute(format!(r#"CREATE DATABASE "{}";"#, config.database_name).as_str())
+        .await
+        .expect("Failed to create database.");
+
+    let db_pool = PgPool::connect(&config.connection_string())
+        .await
+        .expect("failed to connect to postgres");
+
+    sqlx::migrate!("./migrations")
+        .run(&db_pool)
+        .await
+        .expect("Failed to migrate the database");
+
+    db_pool
 }
