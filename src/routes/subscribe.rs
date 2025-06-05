3b064af1a0a2dec58198bfaa0b6c888b703a2a7f
Victor Salles <vsallesa@gmail.com>
2025-06-05 16:24:32 -0300

    feat: implements log and tracing

--- src/routes/subscribe.rs
+++ src/routes/subscribe.rs
@@ -3,16 +3,28 @@
 use actix_web::{HttpResponse, web};
 use chrono::Utc;
 use sqlx::PgPool;
+use tracing::{Instrument, error, info_span};
 use uuid::Uuid;
 
 #[derive(serde::Deserialize)]
-#[allow(dead_code)]
 pub struct SubscriptionData {
     name: String,
     email: String,
 }
 
 pub async fn subscribe(form: web::Form<SubscriptionData>, pool: web::Data<PgPool>) -> HttpResponse {
+    let request_id = Uuid::new_v4();
+    let request_span = info_span!(
+        "Adding a new subscriber.",
+        %request_id,
+        subscriber_email = %form.email,
+        subscriber_name = %form.name
+    );
+
+    let _request_span_guard = request_span.enter();
+
+    let query_span = info_span!("Saving new subscriber in the database");
+
     match sqlx::query!(
         r#"
         INSERT INTO subscriptions (id, email, name, subscribed_at)
@@ -24,15 +36,15 @@ pub async fn subscribe(form: web::Form<SubscriptionData>, pool: web::Data<PgPool
         Utc::now()
     )
     .execute(pool.get_ref())
+    .instrument(query_span)
     .await
     {
-        Ok(_) => {
-            // Subscription successful
-            HttpResponse::Created().finish()
-        }
+        Ok(_) => HttpResponse::Created().finish(),
         Err(e) => {
-            // Handle error, e.g., log it
-            eprintln!("Failed to insert subscription: {}", e);
+            error!(
+                "request_id {} - Failed to insert subscription: {:?}",
+                request_id, e
+            );
             HttpResponse::InternalServerError().finish()
         }
     }
