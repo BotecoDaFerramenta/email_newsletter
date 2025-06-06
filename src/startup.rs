b1338cf68cde5fa5a62eb98f1a3c7e6195af3207
Victor Salles <vsallesa@gmail.com>
2025-06-06 13:27:01 -0300

    feat: enhance web server tracing

--- src/startup.rs
+++ src/startup.rs
@@ -1,15 +1,16 @@
 // !startup.rs
 
 use crate::routes::{health_check, subscribe};
-use actix_web::{App, HttpServer, dev::Server, middleware::Logger, web, web::Data};
+use actix_web::{App, HttpServer, dev::Server, web, web::Data};
 use sqlx::PgPool;
 use std::net::TcpListener;
+use tracing_actix_web::TracingLogger;
 
 pub fn run(listener: TcpListener, db_pool: PgPool) -> Result<Server, std::io::Error> {
     let db_pool = Data::new(db_pool);
     let server = HttpServer::new(move || {
         App::new()
-            .wrap(Logger::default())
+            .wrap(TracingLogger::default())
             .route("/health_check", web::get().to(health_check))
             .route("/subscribe", web::post().to(subscribe))
             .app_data(db_pool.clone())
